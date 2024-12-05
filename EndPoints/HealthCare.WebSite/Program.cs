c1015a91c4ff67faaf44dd4ec966518e0ec7e9be
علیرضا مومنی <alireza.momeni@outlook.com>
2024-12-05 14:49:31 +0100

    di setting

--- EndPoints/HealthCare.WebSite/Program.cs
+++ EndPoints/HealthCare.WebSite/Program.cs
@@ -2,13 +2,11 @@ using HealthCare.Infrastructures.Data.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-// using HealthCare.Infrastructures.DI;
+using HealthCare.Infrastructures.DI;
 using HealthCare.Infrastructures.Shared;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
-//new ContainerBuilder().RegisterModule(new DiHandlerModule());
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,25 +14,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-   .AddCookie(options =>
+    .AddCookie(options =>
     {
-       options.LogoutPath = "/User/logout";
-       options.LoginPath = "/login";
-       options.AccessDeniedPath = "/Home/AccessDenied";
-       options.SlidingExpiration = true;
-       options.ExpireTimeSpan = TimeSpan.FromMinutes(40); 
+        options.LogoutPath = "/User/logout";
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/Home/AccessDenied";
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(40);
     });
 
 builder.Services.AddControllersWithViews();
 
-//builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(
-//    containerBuilder =>
-//        containerBuilder.RegisterModule(new DiHandlerModule())
-//));
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(
+    containerBuilder =>
+        containerBuilder.RegisterModule(new DiHandlerModule())
+));
 builder.Services.AddDbContextPool<HealthCareDbContext>(c =>
     c.UseSqlServer(builder.Configuration.GetConnectionString("HealthCareDb")));
 
- 
+
 var settings = new Settings();
 new ConfigureFromConfigurationOptions<Settings>(builder.Configuration.GetSection("settings")).Configure(settings);
 builder.Services.AddSingleton(settings);
@@ -48,9 +46,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.Urls.Add("http://0.0.0.0:6186");
-}else{
+}
+else
+{
     app.UseHttpsRedirection();
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
--- Infrastructures/HealthCare.Infrastructures.DI/DiHandlerModule.cs
+++ Infrastructures/HealthCare.Infrastructures.DI/DiHandlerModule.cs
@@ -1,6 +1,19 @@
-﻿namespace HealthCare.Infrastructures.DI;
+﻿using Autofac;
+using HealthCare.Framework.Commands;
+using HealthCare.Framework.Queries;
 
-public class DiHandlerModule
+namespace HealthCare.Infrastructures.DI;
+
+public class DiHandlerModule : Module
 {
-    
+    protected override void Load(ContainerBuilder builder)
+    {
+        #region UnitOfWorkDi
+
+        //builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerDependency();
+        builder.RegisterType<CommandDispatcher>().InstancePerDependency();
+        builder.RegisterType<QueryDispatcher>().InstancePerDependency();
+
+        #endregion
+    }
 }
