b86400aab0a5c58ca77ee28471d7ac545744f94d
علیرضا مومنی <alireza.momeni@outlook.com>
2024-12-07 20:32:09 +0100

    remove urls

--- EndPoints/HealthCare.WebSite/Program.cs
+++ EndPoints/HealthCare.WebSite/Program.cs
@@ -46,7 +46,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.Urls.Add("http://0.0.0.0:6186");
+    //app.Urls.Add("http://0.0.0.0:6186");
 }
 else
 {
