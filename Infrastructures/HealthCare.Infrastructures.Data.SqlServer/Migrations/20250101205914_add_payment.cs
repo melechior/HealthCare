c3dcf569f7e579a95e6f5122cc62302a2b856eae
علیرضا مومنی <alireza.momeni@outlook.com>
2025-01-01 21:59:55 +0100

    migration

---
+++ Infrastructures/HealthCare.Infrastructures.Data.SqlServer/Migrations/20250101205914_add_payment.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Infrastructures.Data.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_payment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    ReceiptNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ReceiptDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ShebaNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentDamageFile",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentId = table.Column<long>(type: "bigint", nullable: false),
+                    DamageFileDetailId = table.Column<long>(type: "bigint", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDamageFile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentDamageFile_DamageFileDetails_DamageFileDetailId",
+                        column: x => x.DamageFileDetailId,
+                        principalTable: "DamageFileDetails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PaymentDamageFile_Payment_PaymentId",
+                        column: x => x.PaymentId,
+                        principalTable: "Payment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentDamageFile_DamageFileDetailId",
+                table: "PaymentDamageFile",
+                column: "DamageFileDetailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentDamageFile_PaymentId",
+                table: "PaymentDamageFile",
+                column: "PaymentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDamageFile");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreationDate",
+                value: new DateTime(2024, 12, 21, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
